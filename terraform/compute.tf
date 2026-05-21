22f63c6cb1e8c22db08a273d42829ab8ae180dc0
Evgeny <Ewgen45rus@ya.ru>
2026-05-21 19:49:00 +0500

    Add application virtual machines

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -40,3 +40,177 @@ resource "yandex_compute_instance" "bastion" {
     ssh-keys = local.ssh_metadata
   }
 }
+resource "yandex_compute_instance" "web_1" {
+  name        = "web-1"
+  hostname    = "web-1"
+  platform_id = "standard-v3"
+  zone        = var.zone_a
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 10
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.web_sg.id]
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    ssh-keys = local.ssh_metadata
+  }
+}
+
+resource "yandex_compute_instance" "web_2" {
+  name        = "web-2"
+  hostname    = "web-2"
+  platform_id = "standard-v3"
+  zone        = var.zone_b
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 10
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private_b.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.web_sg.id]
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    ssh-keys = local.ssh_metadata
+  }
+}
+
+resource "yandex_compute_instance" "zabbix" {
+  name        = "zabbix"
+  hostname    = "zabbix"
+  platform_id = "standard-v3"
+  zone        = var.zone_a
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 15
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public_a.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.public_services_sg.id]
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    ssh-keys = local.ssh_metadata
+  }
+}
+
+resource "yandex_compute_instance" "elastic" {
+  name        = "elastic"
+  hostname    = "elastic"
+  platform_id = "standard-v3"
+  zone        = var.zone_d
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 20
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private_d.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.elastic_sg.id]
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    ssh-keys = local.ssh_metadata
+  }
+}
+
+resource "yandex_compute_instance" "kibana" {
+  name        = "kibana"
+  hostname    = "kibana"
+  platform_id = "standard-v3"
+  zone        = var.zone_a
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 10
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public_a.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.public_services_sg.id]
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    ssh-keys = local.ssh_metadata
+  }
+}
