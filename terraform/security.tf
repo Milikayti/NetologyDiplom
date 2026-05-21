112252cda2cb4049eb5e037f216b69771229d07c
Evgeny <Ewgen45rus@ya.ru>
2026-05-21 19:39:36 +0500

    Add service security groups

--- terraform/security.tf
+++ terraform/security.tf
@@ -15,3 +15,105 @@ resource "yandex_vpc_security_group" "bastion_sg" {
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "yandex_vpc_security_group" "web_sg" {
+  name       = "web-sg"
+  network_id = data.yandex_vpc_network.default.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP from internal network"
+    port           = 80
+    v4_cidr_blocks = ["10.10.0.0/16"]
+  }
+
+  ingress {
+    protocol          = "TCP"
+    description       = "ALB healthchecks"
+    port              = 80
+    predefined_target = "loadbalancer_healthchecks"
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion subnet"
+    port           = 22
+    v4_cidr_blocks = ["10.10.1.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "All outgoing"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "public_services_sg" {
+  name       = "public-services-sg"
+  network_id = data.yandex_vpc_network.default.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP"
+    port           = 80
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTPS"
+    port           = 443
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Zabbix web"
+    port           = 8080
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Kibana"
+    port           = 5601
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "All outgoing"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "elastic_sg" {
+  name       = "elastic-sg"
+  network_id = data.yandex_vpc_network.default.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Elasticsearch"
+    port           = 9200
+    v4_cidr_blocks = ["10.10.0.0/16"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion subnet"
+    port           = 22
+    v4_cidr_blocks = ["10.10.1.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "All outgoing"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
